e7a9478f72427e8ec4951af9d31333ab52df7ebf
Stanislav Krotov <stanislav.krotov@devim.team>
2021-10-18 19:20:52 +0300

    1.1.7 изменения:

--- src/ImportResult.php
+++ src/ImportResult.php
@@ -16,6 +16,12 @@ class ImportResult
      */
     private $errorData = [];
 
+    private $selectTime = [];
+
+    private $convertTime = [];
+
+    private $insertTime = [];
+
     /**
      *
      * @return $this
@@ -55,5 +61,51 @@ class ImportResult
     public function getErrorData(): array
     {
         return $this->errorData;
+    }
+
+    public function addSelectTime(float $time): self
+    {
+        $this->selectTime[] = $this;
+        return $this;
+    }
+
+    public function addConvertTime(float $time): self
+    {
+        $this->convertTime[] = $this;
+        return $this;
+    }
+
+    public function addInsertTime(float $time): self
+    {
+        $this->insertTime[] = $time;
+
+        return $this;
+    }
+
+    public function getSelectTime(): float
+    {
+        if (!count($this->selectTime)) {
+            return 0.0;
+        }
+
+        return array_sum($this->selectTime) / count($this->selectTime);
+    }
+
+    public function getConvertTime(): float
+    {
+        if (!count($this->convertTime)) {
+            return 0.0;
+        }
+
+        return array_sum($this->convertTime) / count($this->convertTime);
+    }
+
+    public function getInsertTime(): float
+    {
+        if (!count($this->insertTime)) {
+            return 0.0;
+        }
+
+        return array_sum($this->insertTime) / count($this->insertTime);
     }
 }
